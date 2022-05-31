f19390b36777845ba2843f7d4ac052bf53b7c185
Vũ Đình Phong <vudinhphong.261001@gmail.com>
2022-05-31 12:45:18 +0700

    Add project files.

---
+++ VuDinhPhong_Bai3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VuDinhPhong_Bai3.Startup))]
+namespace VuDinhPhong_Bai3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
